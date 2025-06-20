38ab6d6efc3f454740a300c2346cd06f3dfb3996
sabindangi <sabin4.dangi@gmail.com>
2025-06-19 20:28:32 -0500

    customer-api app

--- customer-api/src/test/java/com/customer/api/customer_api/controller/CustomerControllerTest.java
+++ customer-api/src/test/java/com/customer/api/customer_api/controller/CustomerControllerTest.java
@@ -1,24 +1,32 @@
-package controller;
+package com.customer.api.customer_api.controller;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+import com.customer.api.customer_api.service.CustomerService;
+import java.util.Collections;
+
+@WebMvcTest(CustomerController.class)
 class CustomerControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    
+    @MockitoBean
+    private CustomerService service;
 
     @Test
     void shouldReturnEmptyListInitially() throws Exception {
+        when(service.getAll()).thenReturn(Collections.emptyList());
+
         mockMvc.perform(get("/api/customers"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("[]"));
